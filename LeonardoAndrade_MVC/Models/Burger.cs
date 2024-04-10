756b39620177e84019d9c6422a72c6a8607ca89b
laboratorio <laboratorio@UPOAULA10305>
2024-04-10 11:01:09 -0500

    Agregar archivos de proyecto.

---
+++ LeonardoAndrade_MVC/Models/Burger.cs
@@ -0,0 +1,13 @@
+﻿namespace LeonardoAndrade_MVC.Models
+{
+    public class Burger
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+        public bool WithCheese { get; set; }
+        [Range(0.01, 9999.99)]
+        public decimal Precio { get; set; }
+    }
+}
