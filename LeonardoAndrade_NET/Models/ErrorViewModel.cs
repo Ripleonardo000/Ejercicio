f641e40d71e5c93f4aad58e7c007902a593c7730
Leonardo Andrade <105869866+Ripleonardo000@users.noreply.github.com>
2024-04-17 10:46:46 -0500

    Delete LeonardoAndrade_NET directory

--- LeonardoAndrade_NET/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace LeonardoAndrade_NET.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
