5cd3099f3da4d86583c05edc623a100fc934bd3c
Ripleonardo000 <leonardoandrade2003.197@gmail.com>
2024-04-17 12:15:47 -0500

    commit  2

--- LeonardoAndrade_MVC/Models/Burger.cs
+++ LeonardoAndrade_MVC/Models/Burger.cs
@@ -4,7 +4,7 @@ namespace LeonardoAndrade_MVC.Models
 {
     public class Burger
     {
-       
+        [Key]
         public int BurgerID { get; set; }
 
 
@@ -13,6 +13,6 @@ namespace LeonardoAndrade_MVC.Models
         public bool WithCheese { get; set; }
         [Range(0.01, 9999.99)]
         public decimal Costo { get; set; }
-        public List<Promo> Promo { get; set; }
+        public List<Promo>? Promo { get; set; }
     }
 }
--- LeonardoAndrade_MVC/Models/Promo.cs
+++ LeonardoAndrade_MVC/Models/Promo.cs
@@ -1,14 +1,17 @@
-﻿namespace LeonardoAndrade_MVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeonardoAndrade_MVC.Models
 {
     public class Promo
     {
+        [Key]
         public int PromoID { get; set; }
 
         public string? Descripcion { get; set; }
         public int FechaPromo { get; set; }
 
         public int BurgerID { get; set; }
-        public Burger burger { get; set; }
+        public Burger? burger { get; set; }
 
     }
 }
