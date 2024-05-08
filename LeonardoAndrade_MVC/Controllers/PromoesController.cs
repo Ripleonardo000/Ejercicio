50802f857b8c46d089ee92e8b60e16f762bbbd9f
Ripleo000 <leonardoandrade2003.197@gmail.com>
2024-05-08 10:42:54 -0500

    cambio cambio

---
+++ LeonardoAndrade_MVC/Controllers/PromoesController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LeonardoAndrade_MVC.Data;
+using LeonardoAndrade_MVC.Models;
+
+namespace LeonardoAndrade_MVC.Controllers
+{
+    public class PromoesController : Controller
+    {
+        private readonly LeonardoAndrade_MVCContext _context;
+
+        public PromoesController(LeonardoAndrade_MVCContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Promoes
+        public async Task<IActionResult> Index()
+        {
+            var leonardoAndrade_MVCContext = _context.Promo.Include(p => p.burger);
+            return View(await leonardoAndrade_MVCContext.ToListAsync());
+        }
+
+        // GET: Promoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var promo = await _context.Promo
+                .Include(p => p.burger)
+                .FirstOrDefaultAsync(m => m.PromoID == id);
+            if (promo == null)
+            {
+                return NotFound();
+            }
+
+            return View(promo);
+        }
+
+        // GET: Promoes/Create
+        public IActionResult Create()
+        {
+            ViewData["BurgerID"] = new SelectList(_context.Burger, "BurgerID", "Name");
+            return View();
+        }
+
+        // POST: Promoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PromoID,Descripcion,FechaPromo,BurgerID")] Promo promo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(promo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BurgerID"] = new SelectList(_context.Burger, "BurgerID", "Name", promo.BurgerID);
+            return View(promo);
+        }
+
+        // GET: Promoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var promo = await _context.Promo.FindAsync(id);
+            if (promo == null)
+            {
+                return NotFound();
+            }
+            ViewData["BurgerID"] = new SelectList(_context.Burger, "BurgerID", "Name", promo.BurgerID);
+            return View(promo);
+        }
+
+        // POST: Promoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PromoID,Descripcion,FechaPromo,BurgerID")] Promo promo)
+        {
+            if (id != promo.PromoID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(promo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PromoExists(promo.PromoID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BurgerID"] = new SelectList(_context.Burger, "BurgerID", "Name", promo.BurgerID);
+            return View(promo);
+        }
+
+        // GET: Promoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var promo = await _context.Promo
+                .Include(p => p.burger)
+                .FirstOrDefaultAsync(m => m.PromoID == id);
+            if (promo == null)
+            {
+                return NotFound();
+            }
+
+            return View(promo);
+        }
+
+        // POST: Promoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var promo = await _context.Promo.FindAsync(id);
+            if (promo != null)
+            {
+                _context.Promo.Remove(promo);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PromoExists(int id)
+        {
+            return _context.Promo.Any(e => e.PromoID == id);
+        }
+    }
+}
